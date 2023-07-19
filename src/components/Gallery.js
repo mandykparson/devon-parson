7a97a92876eb16eead07f399db0e45adb3785a4c
Mandy Parson <mandyparson@Mandys-MacBook-Air.local>
2023-07-19 09:02:37 -0600

    checking image latency

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,10 +6,10 @@ export default function Gallery() {
     <Container>
         <Row className="gallery-row">
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0131-0001.jpg" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0131-0001.jpg" fluid rounded /> */}
             </Col>
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0129.jpg" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0129.jpg" fluid rounded /> */}
             </Col>
             <Col>
                 <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0128.jpg" fluid rounded />
@@ -17,24 +17,24 @@ export default function Gallery() {
         </Row>
         <Row className="gallery-row">
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0138.jpg" fluid rounded />   
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0138.jpg" fluid rounded />    */}
             </Col>
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/7c5fd001-81d2-4d9b-bc73-ab89ac3b0e34.JPG" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/7c5fd001-81d2-4d9b-bc73-ab89ac3b0e34.JPG" fluid rounded /> */}
             </Col>
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0130.jpg" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0130.jpg" fluid rounded /> */}
             </Col>
         </Row>
         <Row className="gallery-row">
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/60846fc6-6995-48a6-9a7b-4e8e513ce6a5.JPG" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/60846fc6-6995-48a6-9a7b-4e8e513ce6a5.JPG" fluid rounded /> */}
             </Col>
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/Art.jpg" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/Art.jpg" fluid rounded /> */}
             </Col>
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/7f5ae985-733f-4d15-9c6a-88db12ecf578.JPG" fluid rounded />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/7f5ae985-733f-4d15-9c6a-88db12ecf578.JPG" fluid rounded /> */}
             </Col>
         </Row>
     </Container>
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ export default function Home() {
     <Container className="home-container">
         <Row className="home-row">
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0131-0001.jpg" fluid />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0131-0001.jpg" fluid /> */}
             </Col>
             <Col>
-                <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0129.jpg" fluid />
+                {/* <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0129.jpg" fluid /> */}
             </Col>
             <Col>
                 <Image src="https://img1.wsimg.com/isteam/ip/94ed2665-ba11-4c53-9478-9ef976fc5138/IMG_0138.jpg" fluid />
